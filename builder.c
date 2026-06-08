75037d720e6b1c72c25d3ae7e9c77e6f2042f68a
Peter Sarkoci <petak@dolichocephalus>
2026-06-08 14:12:17 +0200

    all declarations to the top

--- builder.c
+++ builder.c
@@ -6,17 +6,19 @@
 #include <time.h>
 
 void builder(FILE *in, FILE *out, FILE *index){
+     LesData data;
+     IndexHeader header = {0, -1};
+     double total = 0.0;
+     long offset = 0;
+
      if( in == NULL || out == NULL || index == NULL) {
           printf("Error opening file\n");
           return;
      }
 
-     LesData data;
-     IndexHeader header = {0, -1};
      fwrite(&header, sizeof(IndexHeader), 1, index);
-     double total = 0.0;
+   
      fwrite(&total, sizeof(double), 1, index);
-     long offset = 0;
 
      while(fscanf(in, "%s %d %d %lf",
                data.code, 
@@ -29,7 +31,7 @@ void builder(FILE *in, FILE *out, FILE *index){
           
           offset += sizeof(LesData);
           header.count++;
-          }
+     }
 
      total = total_area(index, header.root_offset);
      fseek(index, 0, SEEK_SET);
